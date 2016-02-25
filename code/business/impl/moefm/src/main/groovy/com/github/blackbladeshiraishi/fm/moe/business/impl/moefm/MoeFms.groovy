313f9fddeff4afd15b78506af21942cc44ca78e6
白石 黑刃 <blackblade.shiraishi@outlook.com>
2016-02-25 10:16:43 +0800

    add facade class: MoeFms

---
+++ code/business/impl/moefm/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/business/impl/moefm/MoeFms.groovy
@@ -0,0 +1,18 @@
+package com.github.blackbladeshiraishi.fm.moe.business.impl.moefm
+
+import com.github.blackbladeshiraishi.fm.moe.domain.entity.Radio
+import retrofit2.Retrofit
+
+class MoeFms {
+
+    static Retrofit newRetrofit() {
+        new Retrofit.Builder()
+                .baseUrl("http://moe.fm")
+                .build();
+    }
+
+    static List<Radio> listHotRadios(Retrofit retrofit, String apiKey) {
+        new MoeFmListHotRadios(retrofit: retrofit, apiKey: apiKey).execute()
+    }
+
+}
