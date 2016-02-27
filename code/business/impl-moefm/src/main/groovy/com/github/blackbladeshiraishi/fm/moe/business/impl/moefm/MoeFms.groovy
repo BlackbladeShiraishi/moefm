e437a24a09cd9f5e3f3893393dd0a51819a4cbbe
白石 黑刃 <blackblade.shiraishi@outlook.com>
2016-02-27 17:49:44 +0800

    add Controller and HotRadiosExtension classes

--- code/business/impl-moefm/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/business/impl/moefm/MoeFms.groovy
+++ code/business/impl-moefm/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/business/impl/moefm/MoeFms.groovy
@@ -1,5 +1,6 @@
 package com.github.blackbladeshiraishi.fm.moe.business.impl.moefm
 
+import com.github.blackbladeshiraishi.fm.moe.business.business.ListHotRadios
 import com.github.blackbladeshiraishi.fm.moe.domain.entity.Radio
 import retrofit2.Retrofit
 
@@ -11,8 +12,12 @@ class MoeFms {
         .build();
   }
 
+  static ListHotRadios newListHotRadios(Retrofit retrofit, String apiKey) {
+    new MoeFmListHotRadios(retrofit: retrofit, apiKey: apiKey)
+  }
+
   static List<Radio> listHotRadios(Retrofit retrofit, String apiKey) {
-    new MoeFmListHotRadios(retrofit: retrofit, apiKey: apiKey).execute()
+    newListHotRadios(retrofit, apiKey).execute()
   }
 
 }
