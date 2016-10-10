409cd0952ddf97548e579ee02ba8491ab122babb
白石 黑刃 <blackblade.shiraishi@outlook.com>
2016-10-10 16:32:33 +0800

    add MainLayoutView

--- code/client/android/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/client/android/ui/activity/Main0Activity.java
+++ code/client/android/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/client/android/ui/activity/Main0Activity.java
@@ -1,16 +1,40 @@
 package com.github.blackbladeshiraishi.fm.moe.client.android.ui.activity;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.blackbladeshiraishi.fm.moe.client.android.ui.view.widget.MainLayoutView;
+
+import java.util.Map;
+
+import flow.Direction;
 import flow.Flow;
+import flow.KeyChanger;
+import flow.KeyDispatcher;
+import flow.State;
+import flow.TraversalCallback;
 
 
 public class Main0Activity extends AppCompatActivity {
 
+  // ##### flow #####
   @Override
   protected void attachBaseContext(Context newBase) {
-    newBase = Flow.configure(newBase, this).install();
+    newBase = Flow.configure(newBase, this)
+        .dispatcher(KeyDispatcher.configure(this, new KeyChanger() {
+          @Override
+          public void changeKey(@Nullable State outgoingState, @NonNull State incomingState,
+                                @NonNull Direction direction,
+                                @NonNull Map<Object, Context> incomingContexts,
+                                @NonNull TraversalCallback callback) {
+            //TODO;
+            callback.onTraversalCompleted();
+          }
+        }).build())
+        .install();
     super.attachBaseContext(newBase);
   }
 
@@ -19,6 +43,14 @@ public class Main0Activity extends AppCompatActivity {
     if (!Flow.get(this).goBack()) {
       super.onBackPressed();
     }
+  }
+  // ##### flow end #####
+
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(new MainLayoutView(this));
   }
 
 }
---
+++ code/client/android/src/main/java/com/github/blackbladeshiraishi/fm/moe/client/android/ui/view/widget/MainLayoutView.java
@@ -0,0 +1,107 @@
+package com.github.blackbladeshiraishi.fm.moe.client.android.ui.view.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.github.blackbladeshiraishi.fm.moe.client.android.R;
+
+public class MainLayoutView extends DrawerLayout
+    implements NavigationView.OnNavigationItemSelectedListener {
+
+  private final ViewGroup mainContainer;
+
+  public MainLayoutView(Context context) {
+    super(context);
+  }
+
+  public MainLayoutView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public MainLayoutView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+  }
+
+  // init
+  {
+    setId(R.id.drawer_layout);
+    setFitsSystemWindows(true);
+
+    LayoutInflater inflater = LayoutInflater.from(getContext());
+    inflater.inflate(R.layout.view_main_layout, this);
+    inflater.inflate(R.layout.view_main_layout_nav, this);
+
+    mainContainer = (FrameLayout) findViewById(R.id.main_container);
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    int id = item.getItemId();
+    String selectedName = getResources().getResourceName(id);
+    Toast.makeText(getContext(), "selected: " + selectedName, Toast.LENGTH_SHORT).show();
+    closeDrawer(GravityCompat.START);
+    return true;
+  }
+
+
+  // ########## Input:onBackPressed ##########
+  {
+    // set Focusable to receive KeyEvent
+    setFocusable(true);
+    setFocusableInTouchMode(true);
+  }
+
+  //TODO recheck this method
+  //reference: http://android-developers.blogspot.in/2009/12/back-and-other-hard-keys-three-stories.html
+  @Override
+  public boolean dispatchKeyEvent(KeyEvent event) {
+    if (event.getKeyCode() != KeyEvent.KEYCODE_BACK) {
+      return super.dispatchKeyEvent(event);
+    }
+
+    if (event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0
+        ) {
+
+      // Tell the framework to start tracking this event.
+      getKeyDispatcherState().startTracking(event, this);
+      return true;
+
+    } else if (event.getAction() == KeyEvent.ACTION_UP) {
+      getKeyDispatcherState().handleUpEvent(event);
+      if (event.isTracking() && !event.isCanceled()) {
+
+        // DO BACK ACTION HERE
+        return onBackPressed();
+
+      }
+    }
+    return super.dispatchKeyEvent(event);
+  }
+
+  private boolean onBackPressed() {
+    return closeDrawer();
+  }
+
+  private boolean closeDrawer() {
+    if (isDrawerOpen(GravityCompat.START)) {
+      closeDrawer(GravityCompat.START);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  // ########## Input:onBackPressed End ##########
+
+}
