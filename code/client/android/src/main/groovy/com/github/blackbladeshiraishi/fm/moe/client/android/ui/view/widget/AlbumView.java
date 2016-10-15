0d1f87ba73ceb4e52f8b47c9615b2cd8fbccc614
白石 黑刃 <blackblade.shiraishi@outlook.com>
2016-10-15 12:04:15 +0800

    add tab view to AlbumView

--- code/client/android/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/client/android/ui/view/widget/AlbumView.java
+++ code/client/android/src/main/groovy/com/github/blackbladeshiraishi/fm/moe/client/android/ui/view/widget/AlbumView.java
@@ -3,7 +3,7 @@ package com.github.blackbladeshiraishi.fm.moe.client.android.ui.view.widget;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
@@ -11,9 +11,12 @@ import android.widget.Toast;
 
 import com.github.blackbladeshiraishi.fm.moe.client.android.MoeFmApplication;
 import com.github.blackbladeshiraishi.fm.moe.client.android.R;
-import com.github.blackbladeshiraishi.fm.moe.client.android.ui.adapter.SongsAdapter;
+import com.github.blackbladeshiraishi.fm.moe.client.android.ui.adapter.TabAdapter;
 import com.github.blackbladeshiraishi.fm.moe.domain.entity.Album;
 import com.github.blackbladeshiraishi.fm.moe.domain.entity.Song;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -21,9 +24,7 @@ import rx.schedulers.Schedulers;
 
 public class AlbumView extends FrameLayout {
 
-  private final RecyclerView albumSongListView;
-
-  private final SongsAdapter songsAdapter;
+  private final SongListView albumSongListView;
 
   private Album album;
 
@@ -47,11 +48,16 @@ public class AlbumView extends FrameLayout {
   // init
   {
     LayoutInflater inflater = LayoutInflater.from(getContext());
-    inflater.inflate(R.layout.view_radio_content, this);
-    albumSongListView = (RecyclerView) findViewById(R.id.radio_song_list);
+    inflater.inflate(R.layout.view_radio, this);
+    ViewPager tabsView = (ViewPager) findViewById(R.id.tabs_view);
 
-    songsAdapter = new SongsAdapter();
-    albumSongListView.setAdapter(songsAdapter);
+    // Tab Views
+    List<TabAdapter.Tab> tabs = new ArrayList<>(2);
+    // song list
+    albumSongListView = new SongListView(getContext());
+    tabs.add(new TabAdapter.Tab("曲目", albumSongListView));
+
+    tabsView.setAdapter(new TabAdapter(tabs));
   }
 
   public void setAlbum(Album album) {
@@ -65,8 +71,9 @@ public class AlbumView extends FrameLayout {
     }
     MoeFmApplication.get(getContext()).getAppComponent().getRadioService().albumSongs(album)
         .subscribeOn(Schedulers.io())
+        .toList()
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Observer<Song>() {
+        .subscribe(new Observer<List<Song>>() {
           @Override
           public void onCompleted() {
 
@@ -79,9 +86,8 @@ public class AlbumView extends FrameLayout {
           }
 
           @Override
-          public void onNext(Song song) {
-            songsAdapter.getSongs().add(song);
-            songsAdapter.notifyItemInserted(songsAdapter.getSongs().size() - 1);
+          public void onNext(List<Song> songList) {
+            albumSongListView.setSongList(songList);
           }
         });
   }
