7dfa282278c67f77dd8390c5b1fa132668ca69d5
白石 黑刃 <blackblade.shiraishi@outlook.com>
2016-10-27 15:47:58 +0800

    show loading progress before main page is loaded

--- code/client/android/src/main/java/com/github/blackbladeshiraishi/fm/moe/client/android/ui/view/widget/MainPageView.java
+++ code/client/android/src/main/java/com/github/blackbladeshiraishi/fm/moe/client/android/ui/view/widget/MainPageView.java
@@ -5,10 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.github.blackbladeshiraishi.fm.moe.business.api.entity.MoeFmMainPage;
 import com.github.blackbladeshiraishi.fm.moe.client.android.MoeFmApplication;
+import com.github.blackbladeshiraishi.fm.moe.client.android.R;
 import com.github.blackbladeshiraishi.fm.moe.client.android.ui.adapter.CardClusterViewModelListAdapter;
 import com.github.blackbladeshiraishi.fm.moe.client.android.ui.entity.MoeFmMainPageAdapter;
 import com.github.blackbladeshiraishi.fm.moe.client.android.ui.view.CardClusterViewHolder;
@@ -16,11 +20,11 @@ import com.github.blackbladeshiraishi.fm.moe.client.android.ui.view.CardClusterV
 import java.util.List;
 import java.util.Locale;
 
-import rx.Observer;
+import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 
-public class MainPageView extends RecyclerView {
+public class MainPageView extends FrameLayout {
 
   public static final String NAME = MainPageView.class.getName();
 
@@ -36,11 +40,6 @@ public class MainPageView extends RecyclerView {
 
   public MainPageView(Context context, @Nullable AttributeSet attrs, int defStyle) {
     super(context, attrs, defStyle);
-  }
-
-  // init
-  {
-    setLayoutManager(new LinearLayoutManager(getContext()));
   }
 
   @Override
@@ -59,7 +58,13 @@ public class MainPageView extends RecyclerView {
     unsubscribe();
     subscription = MoeFmApplication.get(getContext()).getAppComponent().getSessionService().mainPage()
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Observer<MoeFmMainPage>() {
+        .subscribe(new Subscriber<MoeFmMainPage>() {
+          @Override
+          public void onStart() {
+            super.onStart();
+            showLoading();
+          }
+
           @Override
           public void onCompleted() {
           }
@@ -73,9 +78,7 @@ public class MainPageView extends RecyclerView {
 
           @Override
           public void onNext(MoeFmMainPage moeFmMainPage) {
-            final List<CardClusterViewHolder.CardClusterViewModel> dataSet =
-                MoeFmMainPageAdapter.newCardClusterViewModelList(moeFmMainPage);
-            setAdapter(new CardClusterViewModelListAdapter(dataSet, 4));
+            showMainPage(moeFmMainPage);
           }
         });
   }
@@ -84,6 +87,27 @@ public class MainPageView extends RecyclerView {
     if (subscription != null) {
       subscription.unsubscribe();
     }
+  }
+
+  private void showLoading() {
+    View v = LayoutInflater.from(getContext()).inflate(R.layout.view_loading_progress, this, false);
+    setContentView(v);
+  }
+
+  private void showMainPage(MoeFmMainPage moeFmMainPage) {
+    RecyclerView recyclerView = new RecyclerView(getContext());
+    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+    final List<CardClusterViewHolder.CardClusterViewModel> dataSet =
+        MoeFmMainPageAdapter.newCardClusterViewModelList(moeFmMainPage);
+    recyclerView.setAdapter(new CardClusterViewModelListAdapter(dataSet, 4));
+
+    setContentView(recyclerView);
+  }
+
+  private void setContentView(View contentView) {
+    removeAllViews();
+    addView(contentView);
   }
 
 }
